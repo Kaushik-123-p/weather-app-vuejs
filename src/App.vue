82187e7c6dda056fabe594b8ffeadec5abb9b55a
kaushik <prajapatikaushik6392@gmail.com>
2025-05-10 18:08:05 +0530

    create weather app on vue js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import {RouterView} from "vue-router"
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div> 
+    <h1>HEllo</h1>
+  </div>
+</template>
